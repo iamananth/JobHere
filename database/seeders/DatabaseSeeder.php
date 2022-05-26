427c5982fa238490b2a9ba8e850012e19d383b3d
iamananth <ananthrshenoy@gmail.com>
2022-05-26 17:13:45 +0530

    images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Listing;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,8 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Listing::create([
-            
-        ]);
+        Listing::factory(6)->create();
     }
 }
