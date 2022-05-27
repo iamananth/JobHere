1562f45b994fcba0dc7784c646ead7e75721b223
iamananth <ananthrshenoy@gmail.com>
2022-05-28 00:36:03 +0530

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,19 +16,10 @@ use App\Models\Listing;
 |
 */
 //all listings
-Route::get('/', function () {
-    return view('listings',[
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //single listing
-Route::get('/listings/{listing}',function(Listing $listing){
-    return view('listing',[
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}',[ListingController::class, 'show']);
 
 // Route::get('/posts/{id}', function($id) {
 //     return response('Post ' . $id);
