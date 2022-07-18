b40f7f297aad7d37bab94270f61544dbde4d5ab3
iamananth <ananthrshenoy@gmail.com>
2022-07-18 15:45:04 +0530

    retaining old value in the form

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -11,7 +11,7 @@
          @csrf
              <div class="mb-6">
                  <label for="company" class="inline-block text-lg mb-2">Company Name</label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" value="{{old('company')}}" />
              </div>
 
              @error('company')
@@ -22,7 +22,7 @@
 
              <div class="mb-6">
                  <label for="title" class="inline-block text-lg mb-2">Job Title</label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title" placeholder="Example: Senior Laravel Developer" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title" placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"  />
              </div>
 
              @error('title')
@@ -33,7 +33,7 @@
 
              <div class="mb-6">
                  <label for="location" class="inline-block text-lg mb-2">Job Location</label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location" placeholder="Example: Remote, Boston MA, etc" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location" placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"  />
              </div>
 
              @error('location')
@@ -44,7 +44,7 @@
 
              <div class="mb-6">
                  <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}"  />
              </div>
 
              @error('email')
@@ -57,7 +57,7 @@
                  <label for="website" class="inline-block text-lg mb-2">
                      Website/Application URL
                  </label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website" value="{{old('website')}}"  />
              </div>
 
              @error('website')
@@ -70,7 +70,7 @@
                  <label for="tags" class="inline-block text-lg mb-2">
                      Tags (Comma Separated)
                  </label>
-                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags" placeholder="Example: Laravel, Backend, Postgres, etc" />
+                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags" placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"  />
              </div>
 
              @error('tags')
@@ -90,7 +90,7 @@
                  <label for="description" class="inline-block text-lg mb-2">
                      Job Description
                  </label>
-                 <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="Include tasks, requirements, salary, etc"></textarea>
+                 <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="Include tasks, requirements, salary, etc" value="{{old('description')}}" ></textarea>
              </div>
 
              @error('description')
